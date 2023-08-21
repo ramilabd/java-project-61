1612d2525e714f47eae11cc0c1802029043d5809
ramilabd <ramsmart@mail.ru>
2023-08-21 23:33:33 +0500

    the application asks for the username

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,14 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
     }
 }
