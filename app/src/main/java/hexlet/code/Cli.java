9a822c2534474513a9363f276ef1ac2980601812
ramilabd <ramsmart@mail.ru>
2023-08-22 13:41:18 +0500

    fixed greeting user

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,15 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void Greeting() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+
+}
